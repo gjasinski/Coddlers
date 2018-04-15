6d0f431ae6b31154ef774f5b55cc298e19fde621
Grzegorz Jasiński <gjasinski@hotmail.com>
2018-04-15 18:23:37 +0200

    Add readme. Add endpoint

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Coddlers-backend
+
+## Development
+
+### Backend
+$ gradle build
+
+$ java -jar build/libs/coddlers-0.0.1-SNAPSHOT.jar
+
+Server listen at localhost:8080
+
+### Frontend
+
+$ npm install
+
+$ ng serve
+
+Server listen at localhost:4200
+
+## Build Fat Jar
+$ gradle clean build
+$ java -jar coddlers-0.0.1-SNAPSHOT.jar
--- src/main/java/pl/coddlers/rest/TestController.java
+++ src/main/java/pl/coddlers/rest/TestController.java
@@ -1,6 +1,7 @@
 package pl.coddlers.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,13 @@ public class TestController {
     public String helloWorld(){
         return "{ " +
                 "\"message\": \"Hello world man!\"" +
+                "}";
+    }
+
+    @GetMapping(path = "/greetings/{name}")
+    public String greetings(@PathVariable(name = "name") String name){
+        return "{ " +
+                "\"message\": \"Hello world " + name + "!\"" +
                 "}";
     }
 }
