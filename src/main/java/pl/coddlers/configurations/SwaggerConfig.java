cc59f90ff426560a77012b9b74b70f35f6d8d5d6
Damian Hanusiak <damianhanusiak@gmail.com>
2018-06-14 15:51:22 +0200

    Update swagger config

--- src/main/java/pl/coddlers/configurations/SwaggerConfig.java
+++ src/main/java/pl/coddlers/configurations/SwaggerConfig.java
@@ -2,6 +2,7 @@ package pl.coddlers.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
@@ -12,12 +13,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Profile("dev")
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .host("http://localhost:8080")
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
