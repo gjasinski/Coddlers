1999fc4e7a705057fa69c1ed986e13dcd5d8cf7a
Grzegorz Jasiński <gjasinski@hotmail.com>
2018-07-25 19:15:12 +0200

    Creating branches in threads

--- src/main/java/pl/coddlers/git/services/GitTaskService.java
+++ src/main/java/pl/coddlers/git/services/GitTaskService.java
@@ -1,89 +1,122 @@
 package pl.coddlers.git.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import pl.coddlers.git.models.Hook;
 import pl.coddlers.git.reposiories.HookRepository;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Slf4j
 @Service
 public class GitTaskService {
-	private static final String MASTER_POSTFIX = "-master";
-	private static final String DEVELOP = "-develop";
-	private static final String REPOSITORY_ID = "repositoryId";
-	private static final String BRANCH = "branch";
-	private static final String REF = "ref";
-	private static final String MASTER = "master";
-	private RestTemplate restTemplate = new RestTemplate();
+    private static final String MASTER_POSTFIX = "-master";
+    private static final String DEVELOP = "-develop";
+    private static final String REPOSITORY_ID = "repositoryId";
+    private static final String BRANCH = "branch";
+    private static final String REF = "ref";
+    private static final String MASTER = "master";
+    private static final String PRIVATE_TOKEN = "private_token";
 
-	@Value("${gitlab.api.host}:${gitlab.api.http.port}${gitlab.api.prefix}")
-	private String gitlabApi;
+    private final HookRepository hookRepository;
 
-	@Value("${gitlab.api.apiuser.private_token}")
-	private String private_token;
+    private RestTemplate restTemplate = new RestTemplate();
 
-	private final HookRepository hookRepository;
+    @Value("${gitlab.api.host}:${gitlab.api.http.port}${gitlab.api.prefix}")
+    private String gitlabApi;
 
-	@Autowired
-	public GitTaskService(HookRepository hookRepository) {
-		this.hookRepository = hookRepository;
-	}
+    @Value("${gitlab.api.apiuser.private_token}")
+    private String private_token;
 
-	public void createTask(long repositoryId, String taskName) {
-		createBranch(repositoryId, taskName + DEVELOP);
-		createBranch(repositoryId, taskName + MASTER_POSTFIX);
-		registerHook(repositoryId, taskName);
-	}
+    @Value("${pl.coddlers.git.http.timeout.milliseconds}")
+    private long timeout;
 
-	private void registerHook(long repositoryId, String taskName) {
-		hookRepository.save(createHook(repositoryId, taskName + MASTER_POSTFIX));
-	}
+    private ExecutorService executor = Executors.newFixedThreadPool(10);
 
-	private void createBranch(long repositoryId, String branchName) {
-		String resourceUrl = createApiUrl(repositoryId);
+    @Autowired
+    public GitTaskService(HookRepository hookRepository) {
+        this.hookRepository = hookRepository;
+    }
 
-		HttpHeaders headers = createHttpHeaders();
+    public void createTask(long repositoryId, String taskName) {
+        Future<Boolean> develop = createBranch(repositoryId, taskName + DEVELOP);
+        Future<Boolean> master = createBranch(repositoryId, taskName + MASTER_POSTFIX);
+        try {
+            Boolean createdDevelop = develop.get(timeout, TimeUnit.MILLISECONDS);
+            Boolean createdMaster = master.get(timeout, TimeUnit.MILLISECONDS);
+            if (createdDevelop && createdMaster) {
+                registerHook(repositoryId, taskName);
+            } else {
+                log.debug("Cannot register hook - branch was not created");
+            }
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            log.error(e.toString());
+        }
+    }
 
-		UriComponentsBuilder builder = createComponentBuilder(resourceUrl)
-				.queryParam(REPOSITORY_ID, repositoryId)
-				.queryParam(BRANCH, branchName)
-				.queryParam(REF, MASTER);
+    private void registerHook(long repositoryId, String taskName) {
+        hookRepository.save(createHook(repositoryId, taskName + MASTER_POSTFIX));
+    }
+
+    private Future<Boolean> createBranch(long repositoryId, String branchName) {
+        return executor.submit(() -> {
+            String resourceUrl = createApiUrl(repositoryId);
+
+            HttpHeaders headers = createHttpHeaders();
+
+            UriComponentsBuilder builder = createComponentBuilder(resourceUrl)
+                    .queryParam(REPOSITORY_ID, repositoryId)
+                    .queryParam(BRANCH, branchName)
+                    .queryParam(REF, MASTER);
 
 
-		HttpEntity<?> entity = new HttpEntity<>(headers);
+            HttpEntity<?> entity = new HttpEntity<>(headers);
 
-		restTemplate.exchange(
-				builder.build().toUriString(),
-				HttpMethod.POST,
-				entity,
-				String.class);
-	}
+            ResponseEntity<String> exchange = restTemplate.exchange(
+                    builder.build().toUriString(),
+                    HttpMethod.POST,
+                    entity,
+                    String.class);
+            if (!exchange.getStatusCode().is2xxSuccessful()) {
+                log.debug(exchange.getStatusCode().toString());
+            }
+            return exchange.getStatusCode().is2xxSuccessful();
+        });
+    }
 
-	private HttpHeaders createHttpHeaders() {
-		HttpHeaders headers = new HttpHeaders();
-		headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-		return headers;
-	}
+    private HttpHeaders createHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        return headers;
+    }
 
-	private String createApiUrl(long repositoryId) {
-		return gitlabApi + " /projects/" + repositoryId + "/repository/branches";
-	}
+    private String createApiUrl(long repositoryId) {
+        return gitlabApi + " /projects/" + repositoryId + "/repository/branches";
+    }
 
-	private Hook createHook(Long projectId, String branchName) {
-		Hook hook = new Hook();
-		hook.setProjectId(projectId);
-		hook.setBranch(branchName);
-		return hook;
-	}
+    private Hook createHook(Long projectId, String branchName) {
+        Hook hook = new Hook();
+        hook.setProjectId(projectId);
+        hook.setBranch(branchName);
+        return hook;
+    }
 
-	private UriComponentsBuilder createComponentBuilder(String resourceUrl) {
-		return UriComponentsBuilder.fromHttpUrl(resourceUrl)
-				.queryParam("private_token", private_token);
-	}
+    private UriComponentsBuilder createComponentBuilder(String resourceUrl) {
+        return UriComponentsBuilder.fromHttpUrl(resourceUrl)
+                .queryParam(PRIVATE_TOKEN, private_token);
+    }
 }
