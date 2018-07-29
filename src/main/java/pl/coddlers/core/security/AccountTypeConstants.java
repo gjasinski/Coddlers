c40a711164aa4b36daa641e8a7d1a681b0e26010
Damian Hanusiak <damianhanusiak@gmail.com>
2018-07-29 12:24:57 +0200

    Replace AccountTypeConstants with AccountTypeEnum

--- src/main/java/pl/coddlers/core/security/AccountTypeConstants.java
+++
@@ -1,9 +0,0 @@
-package pl.coddlers.core.security;
-
-public final class AccountTypeConstants {
-    public static final String ADMIN = "ROLE_ADMIN";
-
-    public static final String STUDENT = "ROLE_STUDENT";
-
-    public static final String TEACHER = "ROLE_TEACHER";
-}
