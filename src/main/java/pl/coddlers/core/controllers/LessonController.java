dbbecfdffbd79b2e01f8b7c1b6a132f31edb6eca
Grzegorz Jasiński <gjasinski@hotmail.com>
2018-07-30 20:35:43 +0200

    Fix missing backslashes. Fix lesson controller

--- src/main/java/pl/coddlers/core/controllers/LessonController.java
+++ src/main/java/pl/coddlers/core/controllers/LessonController.java
@@ -18,58 +18,55 @@ import pl.coddlers.git.services.GitLessonService;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.Collection;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api/lessons")
 public class LessonController {
 
-	private final LessonService lessonService;
+    private final LessonService lessonService;
 
-	private final GitLessonService gitProjectService;
+    private final GitLessonService gitProjectService;
 
-	@Autowired
-	public LessonController(LessonService lessonService, GitLessonService gitProjectService) {
-		this.lessonService = lessonService;
-		this.gitProjectService = gitProjectService;
-	}
+    @Autowired
+    public LessonController(LessonService lessonService, GitLessonService gitProjectService) {
+        this.lessonService = lessonService;
+        this.gitProjectService = gitProjectService;
+    }
 
     @PostMapping
-    public ResponseEntity<Long> createLesson(@Valid @RequestBody LessonDto lessonDto) {
+    public ResponseEntity<Long> createLesson(@Valid @RequestBody LessonDto lessonDto) throws ExecutionException, InterruptedException {
         // TODO this code is only for prototype purposes
         long tutorGitId = 20;
         long studentId = 19;
-        gitProjectService.createLesson(tutorGitId, lessonDto.getTitle())
-                .thenCompose((gitTutorProjectId) -> gitProjectService.forkLesson(gitTutorProjectId, studentId))
-                // TODO only for prototype purposes
-                .thenCompose((gitStudentProjectId) -> CompletableFuture.supplyAsync(() -> {
-                    lessonDto.setGitStudentProjectId(gitStudentProjectId);
-                    return gitStudentProjectId;
-                }));
+        Long gitStudentProjectId = gitProjectService.createLesson(tutorGitId, lessonDto.getTitle())
+                .thenCompose((gitTutorProjectId) -> gitProjectService.forkLesson(gitTutorProjectId, studentId)).get();
+        // TODO only for prototype purposes
+        lessonDto.setGitStudentProjectId(gitStudentProjectId);
 
         Long id = lessonService.createLesson(lessonDto);
 
-		URI location = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(id)
-				.toUri();
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
 
-		return ResponseEntity.created(location).build();
-	}
+        return ResponseEntity.created(location).build();
+    }
 
-	@GetMapping(params = {"courseId"})
-	public ResponseEntity<Collection<LessonDto>> getLessons(@RequestParam(value = "courseId") Long courseId) {
-		return ResponseEntity.ok(lessonService.getAllCoursesLessons(courseId));
-	}
+    @GetMapping(params = {"courseId"})
+    public ResponseEntity<Collection<LessonDto>> getLessons(@RequestParam(value = "courseId") Long courseId) {
+        return ResponseEntity.ok(lessonService.getAllCoursesLessons(courseId));
+    }
 
-	@GetMapping(value = "{id}")
-	public ResponseEntity<LessonDto> getLesson(@PathVariable Long id) {
-		return ResponseEntity.ok(lessonService.getLessonById(id));
-	}
+    @GetMapping(value = "{id}")
+    public ResponseEntity<LessonDto> getLesson(@PathVariable Long id) {
+        return ResponseEntity.ok(lessonService.getLessonById(id));
+    }
 
-	@RequestMapping(method = RequestMethod.PUT, value = "{id}")
-	public ResponseEntity<LessonDto> updateLesson(@PathVariable Long id, @Valid @RequestBody LessonDto lessonDto) {
-		return ResponseEntity.ok(lessonService.updateLesson(id, lessonDto));
-	}
+    @RequestMapping(method = RequestMethod.PUT, value = "{id}")
+    public ResponseEntity<LessonDto> updateLesson(@PathVariable Long id, @Valid @RequestBody LessonDto lessonDto) {
+        return ResponseEntity.ok(lessonService.updateLesson(id, lessonDto));
+    }
 }
