13ac570f6a3fe6ccd166792090fa1db59e0eb195
Damian Hanusiak <damianhanusiak@gmail.com>
2018-10-01 00:23:43 +0200

    Change security settings for git/hooks endpoint

--- src/main/java/pl/coddlers/git/controllers/GitHookController.java
+++ src/main/java/pl/coddlers/git/controllers/GitHookController.java
@@ -2,6 +2,7 @@ package pl.coddlers.git.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +12,19 @@ import pl.coddlers.git.models.event.EventDto;
 @RestController
 @RequestMapping("${pl.coddlers.git.event.url}")
 public class GitHookController {
-	private static final Logger LOGGER = LoggerFactory.getLogger(GitHookController.class);
+	private final Logger logger = LoggerFactory.getLogger(GitHookController.class);
 	private static final String OBJECT_KIND_PUSH = "push";
 
 	@PostMapping
 	public void receiveEvent(@RequestBody EventDto event) {
-		LOGGER.debug("Received event hook: " + event.toString());
-		if (isPushEvent(event) /* TODO: 19.07.18 it is necessary to add more conditions */) {
+		logger.debug("Received event hook: " + event.toString());
+		if (isPushEvent(event) && event.getRef().contains("master")) {
 			// TODO: 19.07.18 add logic
+			logger.debug("push event, master merge");
 		}
 	}
 
 	private boolean isPushEvent(EventDto pushEvent) {
-		return pushEvent.getObjectKind().equals(OBJECT_KIND_PUSH);
+		return pushEvent.getEventName().equals(OBJECT_KIND_PUSH);
 	}
-
 }
