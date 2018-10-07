4af2e52a6786e853e661ba7af5dd6cfa69267e82
Grzegorz Jasiński <gjasinski@hotmail.com>
2018-10-07 20:46:58 +0200

    Refactoring

--- src/main/java/pl/coddlers/core/services/LessonService.java
+++ src/main/java/pl/coddlers/core/services/LessonService.java
@@ -1,7 +1,5 @@
 package pl.coddlers.core.services;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +15,6 @@ import pl.coddlers.core.models.entity.StudentLessonRepository;
 import pl.coddlers.core.models.entity.User;
 import pl.coddlers.core.repositories.CourseVersionRepository;
 import pl.coddlers.core.repositories.LessonRepository;
-import pl.coddlers.core.repositories.StudentLessonRepositoryRepository;
 import pl.coddlers.core.repositories.UserDataRepository;
 import pl.coddlers.git.models.event.ProjectDto;
 import pl.coddlers.git.services.GitLessonService;
@@ -26,15 +23,14 @@ import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class LessonService {
     private static final String DATE_FORMAT = "ddMMyyyyhhmmss";
-    private final StudentLessonRepositoryRepository studentLessonRepositoryRepository;
     private final UserDetailsServiceImpl userDetailsService;
     private final LessonRepository lessonRepository;
     private final LessonConverter lessonConverter;
@@ -45,37 +41,36 @@ public class LessonService {
 
     @Autowired
     public LessonService(UserDetailsServiceImpl userDetailsService, LessonRepository lessonRepository, LessonConverter lessonConverter,
-                         CourseVersionRepository courseVersionRepository, GitLessonService gitProjectService, UserDataRepository userDataRepository, StudentLessonRepositoryRepository studentLessonRepositoryRepository) {
+                         CourseVersionRepository courseVersionRepository, GitLessonService gitProjectService, UserDataRepository userDataRepository) {
         this.userDetailsService = userDetailsService;
         this.lessonRepository = lessonRepository;
         this.lessonConverter = lessonConverter;
         this.courseVersionRepository = courseVersionRepository;
         this.gitProjectService = gitProjectService;
         this.userDataRepository = userDataRepository;
-        this.studentLessonRepositoryRepository = studentLessonRepositoryRepository;
     }
 
-	public Collection<LessonDto> getAllCourseVersionLessons(Long courseId, Integer courseVersionNumber) {
-		CourseVersion courseVersion;
+    public Collection<LessonDto> getAllCourseVersionLessons(Long courseId, Integer courseVersionNumber) {
+        CourseVersion courseVersion;
 
-		if (courseVersionNumber == null) {
-			List<CourseVersion> courseVersionList = courseVersionRepository.findByCourse_IdOrderByVersionNumberDesc(courseId);
-			try {
-				courseVersion = courseVersionList.get(0);
-			} catch (IndexOutOfBoundsException e) {
-				throw new CourseVersionNotFound(courseId, 1);
-			}
-		} else {
-			courseVersion = courseVersionRepository.findByCourse_IdAndVersionNumber(courseId, courseVersionNumber)
-					.orElseThrow(() -> new CourseVersionNotFound(courseId, courseVersionNumber));
-		}
+        if (courseVersionNumber == null) {
+            List<CourseVersion> courseVersionList = courseVersionRepository.findByCourse_IdOrderByVersionNumberDesc(courseId);
+            try {
+                courseVersion = courseVersionList.get(0);
+            } catch (IndexOutOfBoundsException e) {
+                throw new CourseVersionNotFound(courseId, 1);
+            }
+        } else {
+            courseVersion = courseVersionRepository.findByCourse_IdAndVersionNumber(courseId, courseVersionNumber)
+                    .orElseThrow(() -> new CourseVersionNotFound(courseId, courseVersionNumber));
+        }
 
         return getAllCourseVersionLessons(courseVersion.getId());
     }
 
-	private Collection<LessonDto> getAllCourseVersionLessons(long courseVersionId) {
-		return lessonConverter.convertFromEntities(lessonRepository.findByCourseVersionId(courseVersionId));
-	}
+    private Collection<LessonDto> getAllCourseVersionLessons(long courseVersionId) {
+        return lessonConverter.convertFromEntities(lessonRepository.findByCourseVersionId(courseVersionId));
+    }
 
     public Long createLesson(LessonDto lessonDto) {
         try {
@@ -94,33 +89,29 @@ public class LessonService {
         }
     }
 
-
-    public CompletableFuture<List<StudentLessonRepository>> forkModelLesson(CourseEdition courseEdition, Lesson lesson) {
-        // TODO: 30.09.18 error handling
-        List<CompletableFuture<StudentLessonRepository>> listOfCompletableStudentRepositories = cloneLessonForUsersEnrolledToEdition(courseEdition, lesson);
-        return mapListOfFuturesToFutureOfList(listOfCompletableStudentRepositories).thenApply(studentLessonRepositoryRepository::saveAll);
+    public CompletableFuture<StudentLessonRepository> forkModelLessonForUser(CourseEdition courseEdition, Lesson lesson, User user) {
+        return forkLessonForUser(courseEdition, lesson, user);
     }
 
-    public CompletableFuture<List<StudentLessonRepository>> forkRepositories(CourseEdition courseEdition) {
-        // TODO: 30.09.18 error handling
-        List<Lesson> courseEditionLessons = lessonRepository.getCourseEditionLessons(courseEdition.getCourseVersion().getId());
-        List<CompletableFuture<StudentLessonRepository>> collect = courseEditionLessons.stream()
-                .map(lesson -> cloneLessonForUsersEnrolledToEdition(courseEdition, lesson))
-                .reduce(Lists.newArrayList(), reduceSetsToOneSet());
-        return mapListOfFuturesToFutureOfList(collect).thenApply(studentLessonRepositoryRepository::saveAll);
+    public CompletableFuture<List<StudentLessonRepository>> forkModelLesson(CourseEdition courseEdition, Lesson lesson) {
+        List<CompletableFuture<StudentLessonRepository>> listOfCompletableStudentRepositories = cloneLessonForUsersEnrolledToEdition(courseEdition, lesson);
+        return mapListOfFuturesToFutureOfList(listOfCompletableStudentRepositories);
     }
 
     private List<CompletableFuture<StudentLessonRepository>> cloneLessonForUsersEnrolledToEdition(CourseEdition courseEdition, Lesson lesson) {
         return userDataRepository.getCourseEditionUsers(courseEdition.getId()).stream()
-                .map(user -> gitProjectService.forkLesson(lesson, user, courseEdition))
+                .map(user -> forkLessonForUser(courseEdition, lesson, user))
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
 
-    private BinaryOperator<List<CompletableFuture<StudentLessonRepository>>> reduceSetsToOneSet() {
-        return (List<CompletableFuture<StudentLessonRepository>> a, List<CompletableFuture<StudentLessonRepository>> b) -> {
-            a.addAll(b);
-            return a;
-        };
+    private CompletableFuture<StudentLessonRepository> forkLessonForUser(CourseEdition courseEdition, Lesson lesson, User user) {
+        try {
+            return gitProjectService.forkLesson(lesson, user, courseEdition);
+        } catch (Exception ex) {
+            // TODO: 07.10.18 exception logic
+            return null;
+        }
     }
 
     private CompletableFuture<List<StudentLessonRepository>> mapListOfFuturesToFutureOfList(List<CompletableFuture<StudentLessonRepository>> listOfFutureRepositories) {
@@ -137,23 +128,23 @@ public class LessonService {
         return lesson.getCourseVersion().getCourse().getId() + "_" + lesson.getCourseVersion().getId() + "_" + lesson.getId() + "_" + date;
     }
 
-	public LessonDto getLessonById(Long id) {
-		Lesson lesson = validateLesson(id);
+    public LessonDto getLessonById(Long id) {
+        Lesson lesson = validateLesson(id);
 
-		return lessonConverter.convertFromEntity(lesson);
-	}
+        return lessonConverter.convertFromEntity(lesson);
+    }
 
-	public LessonDto updateLesson(Long id, LessonDto lessonDto) {
-		validateLesson(id);
-		lessonDto.setId(id);
-		Lesson lesson = lessonConverter.convertFromDto(lessonDto);
-		lessonRepository.save(lesson);
+    public LessonDto updateLesson(Long id, LessonDto lessonDto) {
+        validateLesson(id);
+        lessonDto.setId(id);
+        Lesson lesson = lessonConverter.convertFromDto(lessonDto);
+        lessonRepository.save(lesson);
 
-		return lessonDto;
-	}
+        return lessonDto;
+    }
 
-	private Lesson validateLesson(Long id) throws LessonNotFoundException {
-		return lessonRepository.findById(id)
-				.orElseThrow(() -> new LessonNotFoundException(id));
-	}
+    private Lesson validateLesson(Long id) throws LessonNotFoundException {
+        return lessonRepository.findById(id)
+                .orElseThrow(() -> new LessonNotFoundException(id));
+    }
 }
