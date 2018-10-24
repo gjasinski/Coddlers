19de0db57ad87fcaa873e3a7832f55ebb5733e8c
Damian Hanusiak <damianhanusiak@gmail.com>
2018-10-25 00:55:10 +0200

    Add logic for lazyRepositoryForking

--- src/main/java/pl/coddlers/core/repositories/CourseEditionLessonRepository.java
+++ src/main/java/pl/coddlers/core/repositories/CourseEditionLessonRepository.java
@@ -1,8 +1,11 @@
 package pl.coddlers.core.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.coddlers.core.models.entity.CourseEditionLesson;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,4 +13,11 @@ public interface CourseEditionLessonRepository extends JpaRepository<CourseEditi
     Optional<CourseEditionLesson> findByLesson_IdAndCourseEdition_Id(long lessonId, long courseEditionId);
 
     List<CourseEditionLesson> findByCourseEdition_Id(long courseEditionId);
+
+    @Query(value = "select * from course_edition_lesson as c " +
+            "where date_part('YEAR', c.start_date) = date_part('YEAR', ?1) and " +
+            "date_part('MONTH', c.start_date) = date_part('MONTH', ?1) and " +
+            "date_part('DAY', c.start_date) = date_part('DAY', ?1)",
+            nativeQuery = true)
+    List<CourseEditionLesson> findByDate(@Param("date") Date date);
 }
--- src/main/java/pl/coddlers/scheduled_tasks/GitScheduledTasks.java
+++ src/main/java/pl/coddlers/scheduled_tasks/GitScheduledTasks.java
@@ -1,15 +1,52 @@
 package pl.coddlers.scheduled_tasks;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import pl.coddlers.core.models.entity.CourseEdition;
+import pl.coddlers.core.models.entity.CourseEditionLesson;
+import pl.coddlers.core.models.entity.Lesson;
+import pl.coddlers.core.repositories.CourseEditionLessonRepository;
+import pl.coddlers.core.repositories.CourseEditionRepository;
+import pl.coddlers.core.repositories.LessonRepository;
+import pl.coddlers.core.services.LessonService;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Date;
 
 @Slf4j
 @Component
 public class GitScheduledTasks {
 
+    private LessonService lessonService;
+    private CourseEditionLessonRepository courseEditionLessonRepository;
+    private CourseEditionRepository courseEditionRepository;
+    private LessonRepository lessonRepository;
+
+    @Autowired
+    public GitScheduledTasks(LessonService lessonService, CourseEditionLessonRepository courseEditionLessonRepository, CourseEditionRepository courseEditionRepository, LessonRepository lessonRepository) {
+        this.lessonService = lessonService;
+        this.courseEditionLessonRepository = courseEditionLessonRepository;
+        this.courseEditionRepository = courseEditionRepository;
+        this.lessonRepository = lessonRepository;
+    }
+
     @Scheduled(cron = "0 50 23 * * 1-7", zone = "CET")
     public void lazyRepositoryForking() {
         log.info("Executed lazy forking task");
+        Date dt = new Date();
+        LocalDateTime localDateTime = LocalDateTime.from(dt.toInstant()).plusDays(1);
+        java.sql.Date sqlDate = java.sql.Date.valueOf(localDateTime.toLocalDate());
+        Collection<CourseEditionLesson> courseEditionLessons = courseEditionLessonRepository.findByDate(sqlDate);
+
+        courseEditionLessons.forEach(courseEditionLesson -> {
+            Lesson lesson = lessonRepository.getOne(courseEditionLesson.getLesson().getId());
+            CourseEdition courseEdition = courseEditionRepository.getOne(courseEditionLesson.getCourseEdition().getId());
+            lessonService.forkModelLesson(courseEdition, lesson).whenComplete(((studentLessonRepositories, throwable) -> {
+                log.info(String.format("Created %d repositories", studentLessonRepositories.size()));
+            }));
+        });
     }
 }
