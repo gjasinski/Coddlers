656d7d3cb5acce40cff7a5ee4ca14dc043914e02
wchmielarz <wchmielarz@protonmail.com>
2018-11-21 20:57:06 +0100

    After review changes

--- src/main/webapp/src/app/components/student/course-edition/course-edition-page/student-course-edition-page.component.ts
+++ src/main/webapp/src/app/components/student/course-edition/course-edition-page/student-course-edition-page.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../../../../models/course";
 import {switchMap} from "rxjs/operators";
-import {forkJoin} from "rxjs";
+import {forkJoin, Observable} from "rxjs";
 import {Lesson} from "../../../../models/lesson";
 import {CourseService} from "../../../../services/course.service";
 import {ActivatedRoute, Router} from "@angular/router";
@@ -13,7 +13,6 @@ import {EventService} from "../../../../services/event.service";
 import {CourseEdition} from "../../../../models/courseEdition";
 import {SubscriptionManager} from "../../../../utils/SubscriptionManager";
 import {SubmissionService} from "../../../../services/submission.service";
-import {Submission} from "../../../../models/submission";
 import {CourseEditionLesson} from "../../../../models/courseEditionLesson";
 import {SubmissionStatusEnum} from "../../../../models/submissionStatusEnum";
 import * as _ from "lodash";
@@ -28,8 +27,8 @@ export class StudentCourseEditionPageComponent implements OnInit {
   courseEdition: CourseEdition;
   lessons: Lesson[] = [];
   editionLessons: Map<Lesson, CourseEditionLesson> = new Map<Lesson, CourseEditionLesson>();
-  submissionsStatus: Map<Lesson, string> = new Map<Lesson, string>();
-  submissionsGrade: Map<Lesson, number> = new Map<Lesson, any>();
+  submissionsStatus: Map<number, string> = new Map<number, string>();
+  submissionsGrade: Map<number, number> = new Map<number, number>();
 
   submitted: number = 0;
   graded: number = 0;
@@ -57,39 +56,41 @@ export class StudentCourseEditionPageComponent implements OnInit {
       this.courseEdition = courseEdition;
       this.course = course;
       this.lessons = lessons;
-
-      this.lessons.forEach((lesson: Lesson) => {
-        let submissionSub = this.submissionService.getSubmissionsForLesson(this.courseEdition.id, lesson.id)
-        .subscribe((submissions: Submission[]) => {
-          if (submissions.length === 0) {
-            this.submissionsStatus.set(lesson, SubmissionStatusEnum.NOT_SUBMITTED.description);
-            this.submissionsGrade.set(lesson, -1);
-          } else {
-            this.submissionsStatus.set(lesson, _.last(submissions).submissionStatus);
-            if (this.submissionsStatus.get(lesson) === SubmissionStatusEnum.NOT_SUBMITTED.description) {
-              this.submissionsGrade.set(lesson, -1);
-            } else {
-              this.submissionsGrade.set(lesson, _.last(submissions).points);
-            }
-          }
-
-          this.submitted = _.size(_.filter(Array.from(this.submissionsStatus.values(),
-            status => status !== SubmissionStatusEnum.NOT_SUBMITTED.description)));
-          this.graded = _.size(_.filter(Array.from(this.submissionsGrade.values()),
-            grade => grade >= 0));
-        });
-
-        let editionLessonSub = this.courseEditionService.getCourseEditionLesson(this.courseEdition.id, lesson.id)
-        .subscribe((editionLessons: CourseEditionLesson) => {
-          this.editionLessons.set(lesson, editionLessons);
-        });
-
-        this.subscriptionManager.add(editionLessonSub);
-        this.subscriptionManager.add(submissionSub);
-      });
+      this.getSubmissionData();
     });
 
     this.subscriptionManager.add(paramsSub);
+  }
+
+  getSubmissionData() {
+    return _.forEach(this.lessons, lesson => {
+      const lessonId = lesson.id;
+      const subscription = forkJoin(
+        this.submissionService.getSubmissionsForLesson(this.courseEdition.id, lessonId),
+        this.courseEditionService.getCourseEditionLesson(this.courseEdition.id, lessonId))
+      .subscribe(([submissions, courseEditionLesson]) => {
+        if (submissions.length === 0) {
+          this.submissionsStatus.set(lessonId, SubmissionStatusEnum.NOT_SUBMITTED.description);
+          this.submissionsGrade.set(lessonId, -1);
+        } else {
+          this.submissionsStatus.set(lessonId, _.last(submissions).submissionStatus);
+          if (this.submissionsStatus.get(lessonId) === SubmissionStatusEnum.NOT_SUBMITTED.description) {
+            this.submissionsGrade.set(lessonId, -1);
+          } else {
+            this.submissionsGrade.set(lessonId, _.last(submissions).points);
+          }
+        }
+
+        this.submitted = _.size(_.filter(Array.from(this.submissionsStatus.values(),
+          status => status !== SubmissionStatusEnum.NOT_SUBMITTED.description)));
+        this.graded = _.size(_.filter(Array.from(this.submissionsGrade.values()),
+          grade => grade >= 0));
+
+        this.editionLessons.set(lesson, courseEditionLesson);
+      });
+
+      this.subscriptionManager.add(subscription);
+    })
   }
 
   ngOnDestroy():
