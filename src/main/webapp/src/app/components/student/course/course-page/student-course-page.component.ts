9926d5fb75c5d2fb495c8d54ec846a9f4feec558
Adam Jamka <adam.jamka.1995@gmail.com>
2018-12-02 01:45:09 +0100

    Added course edition end date to student - my courses page

--- src/main/webapp/src/app/components/student/course/course-page/student-course-page.component.ts
+++ src/main/webapp/src/app/components/student/course/course-page/student-course-page.component.ts
@@ -10,16 +10,28 @@ import {CourseEditionService} from "../../../../services/course-edition.service"
   styleUrls: ['./student-course-page.component.scss']
 })
 export class StudentCoursePageComponent implements OnInit {
-  private courses: CourseWithCourseEdition[];
+  courses: CourseWithCourseEdition[];
+  courseEndDates: Map<CourseWithCourseEdition, Date> = new Map<CourseWithCourseEdition, Date>();
   private subscriptionManager: SubscriptionManager = new SubscriptionManager();
 
   constructor(private courseEditionService: CourseEditionService,
-              private route: ActivatedRoute,) {
+              private route: ActivatedRoute,
+              private editionService: CourseEditionService) {
   }
 
   ngOnInit(): void {
     this.subscriptionManager.add(this.courseEditionService.getCourses()
-      .subscribe((courses: CourseWithCourseEdition[]) => this.courses = courses)
+      .subscribe((courses: CourseWithCourseEdition[]) => {
+        this.courses = courses;
+        this.courses.forEach(course => {
+          this.editionService.getCourseEditionLessonList(course.courseEdition.id).subscribe(courseEditionLessonList => {
+            if (courseEditionLessonList.length == 0)
+              this.courseEndDates.set(course, course.courseEdition.startDate);
+          else
+              this.courseEndDates.set(course, courseEditionLessonList[courseEditionLessonList.length - 1].endDate);
+          })
+        })
+      })
     );
   }
 
